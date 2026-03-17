3e65924a63acdc7ee58e80877ab582b8f8e83a4c
Maximilian Kern <maxkernw@gmail.com>
2026-03-17 10:59:20 +0100

    root url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/tegel-gem/',
+  base: '/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
